330143a33be07520a656a6ae7dfc083be9c85391
faraagon <simme.f@telia.com>
2025-10-20 16:56:30 +0200

    Added sections for inactive users workers by department

--- read-json.ps1
+++ read-json.ps1
@@ -1,7 +1,55 @@
+﻿#Read the JSON-file
 $data = Get-Content -Path "ad_export.json" -Raw | ConvertFrom-Json
 
-Write-Host "==============================="
+#Print out a header with domain and export date
+Write-Host "============================"
 Write-Host " ACTIVE DIRECTORY - MÅNADSRAPPORT"
-Write-Host "==============================="
+Write-Host "============================"
 Write-Host "Domännamn: $($data.domain)"
-Write-Host "Exportdatum: $($data.exportDate)"
+Write-Host "Exportdatum: $($data.export_date)`n"
+
+
+#Find users that are inactive, 30 days+
+Write-Host "Inaktiva användare (senaste 30 dagarna):"
+Write-Host "============================"
+
+
+#Create variable for inactive users
+$inactiveUsers = $data.users | Where-Object {
+    #Convert lastLogon to datetime and compare to todays date minus 30 days
+    ([datetime]$_.lastLogon) -lt (Get-Date).AddDays(-30)
+}
+
+#Loop through the results and print out each inactive user
+foreach ($user in $inactiveUsers) {
+    $daysInactive = ((Get-Date) - [datetime]$user.lastLogon).Days
+    Write-Host "$($user.displayName) $daysInactive dagar inaktiv"
+}
+
+
+#Print the number of inactive users
+Write-Host ""
+Write-Host "Totalt inaktiva användare: $($inactiveUsers.Count)"
+
+
+#Empty counter for workers per department
+$deptCounts = @{} 
+
+#Loop through departments, if it doesn't exist add it. If it exist, +1
+foreach ($user in $data.users) {
+    $dept = $user.department
+
+    if (-not $deptCounts.ContainsKey($dept)) {
+        $deptCounts[$dept] = 0
+    }
+    $deptCounts[$dept]++
+}
+
+#Loop through list and print out result
+Write-Host "Användare per avdelning:"
+Write-Host ""
+Write-Host "============================"
+foreach ($dept in $deptCounts.Keys) {
+    Write-Host "$dept : $($deptCounts[$dept]) användare"
+}
+
